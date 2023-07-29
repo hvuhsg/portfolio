ed8a79dbbd2f8694f80b1cf9e42cd56305fd5c64
hvuhsg <34660046+hvuhsg@users.noreply.github.com>
2023-07-29 22:02:10 +0000

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -112,7 +112,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'halloween',
+    defaultTheme: 'light',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
