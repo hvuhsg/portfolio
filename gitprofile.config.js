b21970c91f8d8769e89423e6e6499dd916340c30
hvuhsg <34660046+hvuhsg@users.noreply.github.com>
2023-07-29 21:51:33 +0000

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['MultiSC', 'QWhaleNotifierClient', 'NetworkScanDetector'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
